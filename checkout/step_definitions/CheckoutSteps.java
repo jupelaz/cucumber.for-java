0820d834d6b0903532c4b794a9dfbaedf17dfa2b
jupelaz <linkguardia@gmail.com>
2022-12-08 16:38:07 +0100

    upload test fixed

--- checkout/step_definitions/CheckoutSteps.java
+++ checkout/step_definitions/CheckoutSteps.java
@@ -1,24 +1,29 @@
 package step_definitions;
 
 import cucumber.api.java.en.*;
-import cucumber.api.PendingException;
+import implementation.Checkout;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
 
 public class CheckoutSteps {
-	@Given("^the price of \"([^\"]*)\" is (\\d+)c$")
-	public void thePriceOfIsC(String arg1, int arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	Map<String, Integer> prices = new HashMap<String, Integer>();
+	Checkout checkout = new Checkout();;
+	@Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
+	public void thePriceOfIsC(String name, int price) throws Throwable {
+		prices.put(name,price);
 	}
 
-	@When("^I checkout (\\d+) ·banana\"$")
-	public void iCheckoutBanana(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@When("^I checkout (\\d+) \"([^\"]*)\"$")
+	public void iCheckoutBanana(int itemCount, String itemName ) throws Throwable {
+		checkout.add(itemCount,prices.get(itemName));
 	}
 
 	@Then("^the total price should be (\\d+)c$")
-	public void theTotalPriceShouldBeC(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void theTotalPriceShouldBeC(int total) throws Throwable {
+	    assertEquals(total, checkout.total());
 	}
 }
